175f2e517f1aa7082a4ce65d7b6c4293c1d7d407
paulafanasyev <paulafanas@duck.com>
2026-09-21 05:40:44 +0300

    fix(web): expand public navigation with ANO and legal sections

--- web/src/components/PublicLayout.jsx
+++ web/src/components/PublicLayout.jsx
@@ -7,6 +7,7 @@ import CookieConsent, { CookieSettingsButton } from './CookieConsent.jsx';
 const NAV = [
   ['/', 'Главная', true],
   ['/about', 'О проекте'],
+  ['/organization', 'АНО ЦПС'],
   ['/calculator', 'Калькулятор'],
   ['/marketplace', 'Маркетплейс'],
   ['/jobs', 'Работа'],
@@ -157,8 +158,8 @@ export default function PublicLayout({ children }) {
             <p>Платформа для самозанятых, ИП и компаний: Светлана, CRM, документы, календарь, маркетплейс и обучение.</p>
           </div>
           <div><h4>Навигация</h4><Link to="/about">О проекте</Link><Link to="/jobs">Работа</Link><Link to="/projects">Проекты Самозанятых</Link><Link to="/education">Обучение</Link><Link to="/blog">Блог</Link></div>
-          <div><h4>Сервисы</h4><Link to="/calculator">Калькулятор</Link><Link to="/marketplace">Маркетплейс</Link><Link to="/grants">Гранты</Link><Link to="/svetlana">Светлана</Link></div>
-          <div><h4>Поддержка</h4><Link to="/contacts">Контакты</Link><Link to="/support">Поддержка</Link><Link to="/faq">FAQ</Link><Link to="/privacy">Конфиденциальность</Link><Link to="/terms">Соглашение</Link><Link to="/cookies">Cookie</Link><CookieSettingsButton /></div>
+          <div><h4>Сервисы</h4><Link to="/calculator">Калькулятор</Link><Link to="/marketplace">Маркетплейс</Link><Link to="/jobs">Работа</Link><Link to="/grants">Гранты</Link><Link to="/svetlana">Светлана</Link></div>
+          <div><h4>Поддержка</h4><Link to="/organization">АНО ЦПС</Link><Link to="/cooperation">Сотрудничество</Link><Link to="/contacts">Контакты</Link><Link to="/support">Поддержка</Link><Link to="/faq">FAQ</Link><Link to="/privacy">Конфиденциальность</Link><Link to="/terms">Соглашение</Link><Link to="/cookies">Cookie</Link><CookieSettingsButton /></div>
         </div>
         <div className="site-container site-footer-bottom">
           <span>© {new Date().getFullYear()} Мир Самозанятых</span>
