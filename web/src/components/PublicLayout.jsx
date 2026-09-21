c3ed77a481c365aa8517b33c322bc17f8dd8e101
paulafanasyev <paulafanas@duck.com>
2026-09-21 06:11:30 +0300

    content: publish AНО ЦПС requisites and restore Svetlana media (#9)
    * legal: add verified AНО ЦПС requisites
    
    * content: add organization mission and requisites
    
    * contact: update public footer for AНО ЦПС
    
    * homepage: restore Svetlana media and project story
    
    * contact: publish verified AНО ЦПС contacts
    
    * seo: align public organization metadata
    
    * style: support Svetlana media and organization story
    
    * media: add user-provided Svetlana photo and welcome video
    
    * content: clarify AНО ЦПС footer mission
    
    * contact: remove legacy organization email
    
    * contact: use AНО ЦПС email across public pages
    
    * legal: add operator contacts to terms and cookies

--- web/src/components/PublicLayout.jsx
+++ web/src/components/PublicLayout.jsx
@@ -154,11 +154,11 @@ export default function PublicLayout({ children }) {
         <div className="site-container site-footer-grid">
           <div>
             <Link className="site-footer-brand" to="/">Мир Самозанятых</Link>
-            <p>Платформа для самозанятых, ИП и компаний: Светлана, CRM, документы, календарь, маркетплейс и обучение.</p>
+            <p>Экосистема поддержки самозанятых: Светлана, рабочие инструменты, работа, обучение, развитие проектов и партнёрские возможности.</p>
           </div>
           <div><h4>Навигация</h4><Link to="/about">О проекте</Link><Link to="/jobs">Работа</Link><Link to="/projects">Проекты Самозанятых</Link><Link to="/education">Обучение</Link><Link to="/blog">Блог</Link></div>
           <div><h4>Сервисы</h4><Link to="/calculator">Калькулятор</Link><Link to="/marketplace">Маркетплейс</Link><Link to="/jobs">Работа</Link><Link to="/grants">Гранты</Link><Link to="/svetlana">Светлана</Link></div>
-          <div><h4>Поддержка</h4><Link to="/organization">АНО ЦПС</Link><Link to="/cooperation">Сотрудничество</Link><Link to="/contacts">Контакты</Link><Link to="/support">Поддержка</Link><Link to="/faq">FAQ</Link><Link to="/privacy">Конфиденциальность</Link><Link to="/terms">Соглашение</Link><Link to="/cookies">Cookie</Link><CookieSettingsButton /></div>
+          <div><h4>АНО ЦПС «Мир Самозанятых»</h4><Link to="/organization">О центре</Link><Link to="/cooperation">Сотрудничество</Link><Link to="/contacts">Контакты</Link><Link to="/support">Поддержка</Link><Link to="/faq">FAQ</Link><Link to="/privacy">Конфиденциальность</Link><Link to="/terms">Соглашение</Link><Link to="/cookies">Cookie</Link><a href="tel:+79199991336">+7 (919) 999-13-36</a><a href="mailto:mir.samozanyatyh@yandex.ru">mir.samozanyatyh@yandex.ru</a><CookieSettingsButton /></div>
         </div>
         <div className="site-container site-footer-bottom">
           <span>© {new Date().getFullYear()} Мир Самозанятых</span>
