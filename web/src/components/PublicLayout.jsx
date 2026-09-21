7915d80c02808286ab5c28b69c6a8f98cfff1cd9
paulafanasyev <paulafanas@duck.com>
2026-09-21 05:27:12 +0300

    fix(web): expose work and self-employed projects in footer

--- web/src/components/PublicLayout.jsx
+++ web/src/components/PublicLayout.jsx
@@ -155,7 +155,7 @@ export default function PublicLayout({ children }) {
             <Link className="site-footer-brand" to="/">Мир Самозанятых</Link>
             <p>Платформа для самозанятых, ИП и компаний: Светлана, CRM, документы, календарь, маркетплейс и обучение.</p>
           </div>
-          <div><h4>Навигация</h4><Link to="/about">О проекте</Link><Link to="/projects">Проекты</Link><Link to="/education">Обучение</Link><Link to="/blog">Блог</Link></div>
+          <div><h4>Навигация</h4><Link to="/about">О проекте</Link><Link to="/jobs">Работа</Link><Link to="/projects">Проекты Самозанятых</Link><Link to="/education">Обучение</Link><Link to="/blog">Блог</Link></div>
           <div><h4>Сервисы</h4><Link to="/calculator">Калькулятор</Link><Link to="/marketplace">Маркетплейс</Link><Link to="/grants">Гранты</Link><Link to="/svetlana">Светлана</Link></div>
           <div><h4>Поддержка</h4><Link to="/contacts">Контакты</Link><Link to="/support">Поддержка</Link><Link to="/faq">FAQ</Link><Link to="/privacy">Конфиденциальность</Link></div>
         </div>
